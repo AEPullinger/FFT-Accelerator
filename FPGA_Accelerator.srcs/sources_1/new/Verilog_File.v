8a5b51afd695ccb01ae1e559930631deadda8b9e
AEPullinger <89876443+AEPullinger@users.noreply.github.com>
2025-11-06 15:01:00 -0600

    Add files via upload

---
+++ FPGA_Accelerator.srcs/sources_1/new/Verilog_File.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: None
+// Engineer: Amanda Pullinger
+// 
+// Create Date: 10/02/2025 05:15:46 PM
+// Design Name: 
+// Module Name: Verilog_File
+// Project Name: FPGA 3D FFT Accelerator
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Verilog_File(
+
+    );
+endmodule
